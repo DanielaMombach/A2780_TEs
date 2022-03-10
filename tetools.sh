fdd2da4b131f21cddd951ccd024e3cd6a2f55eac
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-03-10 08:53:56 -0300

    new HD

--- tetools.sh
+++ tetools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Master paths
-DANIP="/home/labdros/labdros04/danidoc"
-TETOOLS="/home/labdros/labdros04/danidoc/TEtools"
+DANIP="/media/labdros/Daniela"
+TETOOLS="/home/labdros/software/tetools-master"
 
 # Essential files
 ROSETTE="$DANIP/rosette_hg19.txt"
@@ -18,12 +18,11 @@ FQ_06="$DANIP/SRR14310045.fastq"
 
 # TE output
 COUNT_FILE="count_out.txt"
-HTML_FILE="$DANIP/result_diff.html"
+HTML_FILE="$DANIP/"
 DIFF_OUTDIR="$DANIP/"
 
 # TE count
 python3 $TETOOLS/TEcount.py -bowtie2 -rosette $ROSETTE -column 1 -TE_fasta $TE_FASTA -count $COUNT_FILE -RNA $FQ_01 $FQ_02 $FQ_03 $FQ_04 $FQ_05 $FQ_06
 # TE diff
-Rscript $TETOOLS/TEdiff.R --args --FDR_level=0.05 --count_column=1 --count_file=\"$COUNT_FILE\" --experiment_formula=\"Treatment:Replicate\" --sample_names=\"A2780:Rep1,A2780:Rep2,A2780:Rep3,A2780t:Rep1,A2780t:Rep2,A2780t:Rep3" --outdir=\"$DIFF_OUTDIR\" --htmlfile=\"$HTML_FILE\"
-
+Rscript $TETOOLS/TEdiff.R --args --FDR_level=0.05 --count_column=1 --count_file=\"$COUNT_FILE\" --experiment_formula=\"Treatment:Replicate\" --sample_names=\"A2780:Rep1,A2780:Rep2,A2780:Rep3,A2780t:Rep1,A2780t:Rep2,A2780t:Rep3\" --outdir=\"$DIFF_OUTDIR\" --htmlfile=\"$HTML_FILE\"
 echo "Done"
