74405f0b9ad2c45323faf0f548424a45747bc917
Daniela Moreira Mombach <69865454+DanielaMombach@users.noreply.github.com>
2022-07-12 09:39:29 -0300

    matrix

--- PCA.R
+++ PCA.R
@@ -1,5 +1,5 @@
 ##Input count table
-counttable = read.csv("counttable_PCA.csv", header = TRUE, row.names = 1, sep = ",")
+counttable = read.csv("teste_pca.csv", header = TRUE, row.names = 1, sep = ",")
 head(counttable)
 counttable = round(counttable,digits = 0)
 head(counttable)
@@ -20,7 +20,7 @@ experiment_formula="Sample:Replicate"
 sample_names="A2780 cont:Replicate 1, A2780cont:Replicate 2, A2780cont:Replicate 3, A2780 cisplatin:Replicate 1, A2780 cisplatin:Replicate 2, A2780 cisplatin:Replicate 3, Acis cont:Replicate 1, Acis cont:Replicate 2, Acis cont:Replicate 3, Acis cisplatin:Replicate 1, Acis cisplatin:Replicate 2, Acis cisplatin:Replicate 3"
 
 # we get the counts
-counts = read.table("counttable_PCA.csv")
+counts = read.table("teste_pca.csv")
 rownames(counts) = counts[,1]
 counts_information = counts[, c(1:count_column) ]
 counts_total = counts[, dim(counts)[2]]
@@ -45,10 +45,11 @@ variables
 
 library(DESeq2)
 ntop = 500
+dds = data.matrix(counttable, rownames.force = NA) ##counttable to matrix
 rld = rlogTransformation(dds, blind=T)
-rv = rowVars(assay(rld))
+rv = rowVars(rld)
 select = order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))]
-pca = prcomp(t(assay(rld)[select, ]))
+pca = prcomp(t(rld))
 
 library(ggplot2)
 x = ggplot(data=as.data.frame(pca$x), aes(PC1, PC2, color=variables[,1], shape=variables[,2] )) +
